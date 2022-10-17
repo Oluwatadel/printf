9b83cfcc4175009c07f8a39c763a438e8c5ce491
Oluwatadel <oluwatobiadelesi>
2022-10-17 12:05:54 +0000

    putchar

--- _putchar.c
+++ _putchar.c
@@ -2,11 +2,11 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: The character to print
+ * @chr: character to display
  *
  * Return: On success 1.On error, -1 is returned
  */
 int _putchar(char chr)
 {
-return (write(1, &chr, 1));
+	return (write(1, &chr, 1));
 }
